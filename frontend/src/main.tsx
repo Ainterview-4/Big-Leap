85585aaf1f7ddfce2796e9757b1b406dc73467d3
Beşir Can Barutçu <147250697+BesircanB@users.noreply.github.com>
2025-12-04 12:09:47 +0000

    Fix merge and lint errors

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -8,11 +8,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <App />
-    </ThemeProvider>
-  </React.StrictMode>
-);
+const root = document.getElementById("root");
+
+if (root) {
+  ReactDOM.createRoot(root).render(
+    <React.StrictMode>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <App />
+      </ThemeProvider>
+    </React.StrictMode>
+  );
+}
