5755541cebe005f7b82ecd94ddab5832a95a0bd3
Beşir Can Barutçu <147250697+BesircanB@users.noreply.github.com>
2025-12-04 15:55:12 +0300

    fix: import errors after merge

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,17 +1,14 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import { createRoot } from "react-dom/client";
 import App from "./App";
 import { ThemeProvider } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
 import theme from "./theme/muiTheme";
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
 
 const root = document.getElementById("root");
 
 if (root) {
-  ReactDOM.createRoot(root).render(
+  createRoot(root).render(
     <React.StrictMode>
       <ThemeProvider theme={theme}>
         <CssBaseline />
