02bb456d11a14e0dc3d00b21829e6aa2526cede3
kdrbaserr <155307684+kdrbaserr@users.noreply.github.com>
2025-12-14 14:35:05 +0300

    ERROR HANDLER (#83)
    * ERROR HANDLER
    
    * problem  is solved

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -4,12 +4,16 @@ import path from "path";
 import fs from "fs";
 import swaggerUi from "swagger-ui-express";
 import YAML from "yamljs";
+import dotenv from "dotenv";
+import { errorHandler } from "./middlewares/errorHandler";
+
+dotenv.config();
 
 const app = express();
 
 // Middleware
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: process.env.CORS_ORIGIN || "http://localhost:5173",
   credentials: true
 }));
 app.use(express.json());
@@ -40,6 +44,9 @@ app.use("/auth", authRoutes);
 app.get("/", (req, res) => {
   res.json({ message: "Backend API is running!" });
 });
+
+// Error handler
+app.use(errorHandler);
 
 // Start server
 const PORT = process.env.PORT || 5001;
